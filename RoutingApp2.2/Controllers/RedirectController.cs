474b41fdb50a81e5834f20c0ae1f999f389bddfa
Jake Barron <jake.barron@epitomy.com>
2019-12-06 10:25:09 +0000

    add code

---
+++ RoutingApp2.2/Controllers/RedirectController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RoutingApp2.Controllers
+{    
+    [Route("redirect/{someIdentifier}")]
+    public class RedirectController : Controller
+    {
+        [HttpGet]
+        [Route("initroute")]
+        public IActionResult InitRoute(string someIdentifier)
+        {
+            return RedirectToAction("NextRoute", "Redirect");
+        }
+
+        [HttpGet]
+        [Route("nextroute")]
+        public IActionResult NextRoute(string someIdentifier)
+        {
+            Debug.WriteLine(someIdentifier);
+
+            return View();
+        }
+    }
+}
---
+++ RoutingApp3.0/Controllers/RedirectController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RoutingApp3.Controllers
+{
+
+    [Route("redirect/{someIdentifier}")]
+    public class RedirectController : Controller
+    {        
+        [HttpGet]
+        [Route("initroute")]
+        public IActionResult InitRoute(string someIdentifier)
+        {          
+            return RedirectToAction("NextRoute", "Redirect");
+        }
+
+        [HttpGet]
+        [Route("nextroute")]
+        public IActionResult NextRoute(string someIdentifier)
+        {
+            Debug.WriteLine(someIdentifier);
+
+            return View();
+        }
+    }
+}
